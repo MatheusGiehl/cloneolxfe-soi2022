fa95faef0a7aba415ff7e16548588e2d7ca7a905
Matheus <giehlmatheus96@gmail.com>
2022-09-05 21:11:05 -0300

    update Header styled

--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -32,4 +32,46 @@ export const HeaderArea = styled.div`
      .logo-2 { color: #00FF00;}
      .logo-3 { color: #0000FF;}
     }
+
+    nav {
+     padding-top: 10px;
+     padding-bottom: 10px;
+     
+     ul, li {
+          margin: 0;
+          padding: 0;
+          list-style: none;
+     }
+
+     ul {
+          display: flex;
+          align-items: center;
+          height: 40px;
+     }
+
+     li {
+          margin-left: 20px;
+          margin-right: 20px;
+          
+          a {
+               color: #000000;
+               font-size: 14px;
+
+               &:hover {
+                    color: #999;
+               }
+
+               &.button {
+                    background-color: #FF8100;
+                    border-radius: 4px;
+                    color: #FFFFFF;
+                    padding: 5px 10px;
+               }
+
+               &.button:hover {
+                    background-color: #E57706;
+               }
+          }
+     }
+    }
 `

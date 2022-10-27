a7f1de58c713063cad3a7d2f9a0adf03e4faafe9
Matheus <giehlmatheus96@gmail.com>
2022-10-27 19:45:24 -0300

    update routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,47 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch } from 'react-router-dom';
 
-import Home from './pages/Home'
-import About from './pages/About'
-import NotFound from './pages/NotFound'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import AdPage from './pages/AdPage';
+import RouterHandler from './components/RouterHandler';
+import AddAd from './pages/AddAd';
+import Ads from './pages/Ads';
+import MyAccount from './pages/MyAccount';
 
 export default () => {
     return(
-        <Routes>
-            <Route exact path="/" element={<Home/>} />
-            <Route exact path="/about" element={<About/>} />
-            <Route exact path="/signin" element={< SignIn />} />
-            <Route exact path="/signup" element={<SignUp />} />
-            <Route path="*" element={<NotFound/>} />
-        </Routes>
+        <Switch>
+        <RouterHandler exact path="/">
+             <Home/>
+        </RouterHandler>
+        <RouterHandler exact path="/about">
+             <About/>
+         </RouterHandler>
+        <RouterHandler exact path="/signin">
+            <SignIn/>
+        </RouterHandler>
+        <RouterHandler exact path="/signup">
+            <SignUp/>
+        </RouterHandler>
+        <RouterHandler exact path="/ad/:id">
+            <AdPage/>
+        </RouterHandler>
+        <RouterHandler exact path="/ads">
+            <Ads/>
+        </RouterHandler>
+        <RouterHandler  exact path="/post-an-ad">
+            <AddAd/>
+        </RouterHandler>
+        <RouterHandler private exact path="/myacount">
+            <MyAccount/>
+        </RouterHandler>
+        <RouterHandler path="*">
+            <NotFound/>
+        </RouterHandler>
+        </Switch>
     )
 }
